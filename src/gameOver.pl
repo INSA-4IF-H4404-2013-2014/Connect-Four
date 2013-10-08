4c741c8ee8e1e4cba08cd72562222c8235d43498
Martin Wetterwald <martin.wetterwald@insa-lyon.fr>
2013-10-08 11:26:11 +0200

    gridIsFull orienté objet terminée et testée

--- src/gameOver.pl
+++ src/gameOver.pl
@@ -1,34 +1,20 @@
 :- [gameGrid].
 
-% Checks if the grid is full
-% Just call gridIsFull from outside.
-privateGridIsFull(7) :- gameGrid(7, 6, _). 
-privateGridIsFull(Number) :- gameGrid(Number, 6, _), Next is Number + 1, privateGridIsFull(Next).
-gridIsFull :- privateGridIsFull(1).
+gridIsFull([]).
+gridIsFull([T|M]) :- length(T,N), linesNumber(LN), N == LN, gridIsFull(M).
 
 % Checks if the game is over
 gameOver(1) :- playerWon(1).
 gameOver(2) :- playerWon(2).
 gameOver(0) :- columnsNumber(X), gridIsFull(X).
 
+fullGrid([[1,1,1,2,1,1],[1,1,1,1,2,1],[1,2,1,1,1,1],[1,1,1,1,1,1],[1,1,1,1,1,2],[1,1,1,1,1,1],[1,1,1,1,1,1]]).
 
+almostFullGrid([[1,1,1,2,1,1],[1,1,1,1,2,1],[2,1,2,1,1,1],[1,1,1,1,1,1],[1,1,1,1,2],[1,1,1,1,1,1],[1,1,1,1,1,1]]).
 
 testGridIsFull :-
-	not(gridIsFull),
-	assert(gameGrid(1,6,1)),
-	assert(gameGrid(2,6,1)),
-	assert(gameGrid(3,6,1)),
-	assert(gameGrid(4,6,1)),
-	assert(gameGrid(5,6,1)),
-	assert(gameGrid(6,6,1)),
-	assert(gameGrid(7,6,1)),
-	gridIsFull,
-	retract(gameGrid(1,6,1)),
-	not(gridIsFull),
-	assert(gameGrid(1,6,1)),
-	gridIsFull,
-	retract(gameGrid(4,6,2)),
-	not(gridIsFull).
+	fullGrid(X), gridIsFull(X),
+	almostFullGrid(Y), not(gridIsFull(Y)).
 
 test :-
 	testGridIsFull.
