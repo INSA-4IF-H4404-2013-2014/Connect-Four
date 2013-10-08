b05c49ef73ff1359999ac1c0f660ee71b3fcbf09
Nicolas Buisson <nicolas.buisson@insa-lyon.fr>
2013-10-08 11:46:51 +0200

    Game launcher plays the game, instead of the AIs + add Grid as parameter

--- src/launcher.pl
+++ src/launcher.pl
@@ -1,12 +1,28 @@
+:- [gameGrid].
+
+iaTest1(_, 1).
+iaTest2(_, 2).
+
+gameOver(Grid, Result) :- gameGridGet(Grid, Result, 4, Result).
+
+
+launch(Player1, Player2, Result) :- write('Debut'), gameNewGrid(Grid), launch(Grid, Player1, Player2, Result, 1).
+
 % Launch the game with Player1 and Player2 as predicate names.
 % Result is the ID of the winning player or 0 if no player wins.
 
 % Check for the first time if the game is over
-launch(Player1, Player2, Result) :- gameOver(Result).
+launch(Grid, Player1, Player2, Result, _) :- write('Game over ?'), gameOver(Grid, Result).
 
 % Let the Player1 play, check if the game is over, if not, let the Player2 play and makes a new call to launch
-launch(Player1, Player2, Result) :- 
-	call(Player1), not(gameOver(Result)), call(Player2), launch(Player1, Player2, Result).
+launch(Grid, Player1, Player2, Result, 1) :- 
+	write('1'),
+	call(Player1(Grid), NumCol),
+	gamePlay(Grid, NumCol, 1, ResGrid),
+	launch(ResGrid, Player1, Player2, Result, 2).
 	
-% Get the Result of the game once Player1 finished the game (otherwise, we would just get a false statement)
-launch(Player1, Player2, Result) :- gameOver(Result).
+launch(Grid, Player1, Player2, Result, 2) :- 
+	write('2'),
+	call(Player2(Grid), NumCol),
+	gamePlay(Grid, NumCol, 2, ResGrid),
+	launch(ResGrid, Player1, Player2, Result, 1).
