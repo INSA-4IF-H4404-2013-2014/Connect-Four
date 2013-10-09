08d1656277981474f9951916b4ea1072b75a0a31
Guillaume Abadie <guillaume.abadie@gmail.com>
2013-10-09 19:32:13 +0200

    Implements gamePrintGrid to print a grid in the console

--- src/gameGrid.pl
+++ src/gameGrid.pl
@@ -76,3 +76,32 @@ gameIsValidePlay(Grid, ColumnId) :-
     listFetch(Grid, ColumnId, Column) ->
         length(Column, ColumnHeight),
         not(linesNumber(ColumnHeight)).
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% PRINT A GRID
+% Print a grid
+% gamePrintGrid(Grid)
+privateGamePrintSymbols(1,x).
+privateGamePrintSymbols(2,o).
+
+privateGamePrintGrid([], _).
+
+privateGamePrintGrid([Column|Grid], LineId) :-
+    (listFetch(Column, LineId, Result) ->
+        privateGamePrintSymbols(Result, S), write(S), write(' ') ;
+        write('  ')
+    ) ->
+    privateGamePrintGrid(Grid, LineId).
+
+gamePrintGrid(_, 0).
+
+gamePrintGrid(Grid, LineId) :-
+    write('| ') ->
+    privateGamePrintGrid(Grid, LineId) ->
+    write('\n') ->
+    LineId1 is LineId - 1 ->
+    gamePrintGrid(Grid, LineId1).
+
+gamePrintGrid(Grid) :-
+    linesNumber(LinesCount),
+    gamePrintGrid(Grid, LinesCount), !.
