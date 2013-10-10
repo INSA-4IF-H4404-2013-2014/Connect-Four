ff9a6606ff824685f9d22960cf0032e8008747c2
Martin Wetterwald <martin.wetterwald@insa-lyon.fr>
2013-10-10 17:01:29 +0200

    gameOver refactored. gameOver/DrawMatch tests passed
    Still some work left on gameOver

--- src/gameOver.pl
+++ src/gameOver.pl
@@ -6,18 +6,23 @@ gameGridIsFull([]).
 gameGridIsFull([T|M]) :- length(T,N), linesNumber(LN), N == LN, gameGridIsFull(M).
 
 % Check if there is 4 pawns of the same color in one diagonal
-% privateWonDiagonal(P,M) :-
+privateGamePlayerWonStarCheckDiagonal(Matrix, LastColumnPlayed, TopLine, CurrentLine, Player) :- fail.
+
+% Check if there is 4 pawns of the same color in one line
+privateGamePlayerWonStarCheckLine(Matrix, LastColumnPlayed, CurrentColumn, TopLine, Player) :- fail.
 
 % Check if there is 4 pawns of the same color in one column
-% privateWonColumn(P,M) :-
+privateGamePlayerWonStarCheckColumn(Matrix, LastColumnPlayed, TopLine, CurrentLine, Player) :- fail.
 
 % Check if a player won (P is the player, M the grid)
-gamePlayerWon(P,M) :- privateGameWonDiagonal(P,M).
-gamePlayerWon(P,M) :- privateGameWonLine(P,M).
-gamePlayerWon(P,M) :- privateGameWonColumn(P,M).
+gamePlayerWon(Matrix, LastColumnPlayed, TopLine, Player) :- privateGamePlayerWonStarCheckDiagonal(Matrix, LastColumnPlayed, TopLine, TopLine, Player).
+gamePlayerWon(Matrix, LastColumnPlayed, TopLine, Player) :- privateGamePlayerWonStarCheckLine(Matrix, LastColumnPlayed, LastColumnPlayed, TopLine, Player).
+gamePlayerWon(Matrix, LastColumnPlayed, TopLine, Player) :- privateGamePlayerWonStarCheckColumn(Matrix, LastColumnPlayed, TopLine, TopLine, Player).
 
 
 % Checks if the game is over
-gameOver(1) :- gamePlayerWon(1).
-gameOver(2) :- gamePlayerWon(2).
-gameOver(0) :- columnsNumber(X), gameGridIsFull(X).
+gameOver(Matrix, LastColumPlayed, Player) :- 
+	gameColumnHeight(Matrix, LastColumnPlayed, TopLine),
+	gameGridGet(Matrix, LastColumPlayed, TopLine, Player),
+	gamePlayerWon(Matrix, LastColumnPlayed, TopLine, Player).
+gameOver(Matrix, LastColumPlayed, 0) :- gameGridGet(Matrix, LastColumPlayed, 1, _), gameGridIsFull(Matrix).
--- src/testGameOver.pl
+++ src/testGameOver.pl
@@ -1,6 +1,7 @@
 
 :- [testUtil].
 :- [gameOver].
+:- [gamePrint].
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% UNIT TESTS
@@ -22,7 +23,27 @@ testPrivateAlmostFullGrid([
     [1,1,1,1,2],
     [1,1,1,1,1,1],
     [1,1,1,1,1,1]]).
+	
+testPrivateDrawMatchGrid1([
+	[2,1,2,2,1,1],
+	[1,2,1,1,2,1],
+	[2,1,2,2,2,1],
+	[1,2,1,1,1,2],
+	[2,1,1,2,1,1],
+	[2,1,2,1,2,2],
+	[2,1,2,2,1,2]
+]).
 
+testPrivateDrawMatchGrid2([
+	[2,2,2,1,1,1],
+	[1,1,1,2,2,2],
+	[2,2,2,1,1,1],
+	[1,1,1,2,2,2],
+	[2,2,2,1,1,1],
+	[1,1,1,2,2,2],
+	[2,2,2,1,1,1]
+]).
+	
 testGridIsFull :-
     testPrivateFullGrid(X),
     testPrivateAlmostFullGrid(Y),
@@ -30,5 +51,17 @@ testGridIsFull :-
     not(gameGridIsFull(Y)),
     not(gameGridIsFull([[], [], [], [], [], [], []])).
 
+testGameOver :- 
+	testPrivateDrawMatchGrid1(M),
+	gameOver(M, 1, 0),
+	gameOver(M, 2, 0),
+	gameOver(M, 3, 0),
+	gameOver(M, 4, 0),
+	gameOver(M, 5, 0),
+	gameOver(M, 6, 0),
+	gameOver(M, 7, 0),
+	not(gameOver(M, 45, 0)).
+	
 testAllGameOver :-
-    test(testGridIsFull).
+    test(testGridIsFull),
+	test(testGameOver).
