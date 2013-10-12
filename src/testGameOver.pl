ed2a0ed3aa212c5aa5310a80deea993dc252e06d
Martin Wetterwald <martin.wetterwald@insa-lyon.fr>
2013-10-12 11:30:09 +0200

    Adds additional unit tests for privateGamePlayerWonStarCheckColumn

--- src/testGameOver.pl
+++ src/testGameOver.pl
@@ -1,7 +1,6 @@
 
 :- [testUtil].
 :- [gameOver].
-:- [gamePrint].
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% UNIT TESTS
@@ -64,22 +63,41 @@ testPrivateNotWon1([
 	[2,2,1],
 	[]
 ]).
+
+%This can be reused for all directions
+testPrivateWonColumnNotWonOthers([
+	[2,2,2],
+	[1,1,2],
+	[2,2,1],
+	[1,1,1,1],
+	[2,2,1],
+	[1,1,2],
+	[2,2,2]
+]).
 	
 testGridIsFull :-
     testPrivateFullGrid(X),
     testPrivateAlmostFullGrid(Y),
+	testPrivateWonColumnNotWonOthers(Z),
     gameGridIsFull(X),
     not(gameGridIsFull(Y)),
-    not(gameGridIsFull([[], [], [], [], [], [], []])).
+    not(gameGridIsFull([[], [], [], [], [], [], []])),
+	not(gameGridIsFull(Z)).
 
 testPrivateGamePlayerWonStarCheckColumn :-
 	testPrivateDrawMatchGrid2(M),
+	testPrivateWonPlayer(M2),
+	testPrivateNotWon1(M3),
+	testPrivateWonColumnNotWonOthers(M4),
 	linesNumber(LN),
 	not(privateGamePlayerWonStarCheckColumn(M, 2, LN, LN, 1)),
-	testPrivateWonPlayer(M2),
 	privateGamePlayerWonStarCheckColumn(M2, 1, 5, 5, 1),
-	testPrivateNotWon1(M3),
-	not(privateGamePlayerWonStarCheckColumn(M3, 1, 3, 3, 2)).
+	not(privateGamePlayerWonStarCheckColumn(M3, 1, 3, 3, 2)),
+	not(privateGamePlayerWonStarCheckColumn(M4, 1, 3, 3, 2)),
+	not(privateGamePlayerWonStarCheckColumn(M4, 7, 3, 3, 1)),
+	not(privateGamePlayerWonStarCheckColumn(M4, 7, 3, 3, 2)),
+	not(privateGamePlayerWonStarCheckColumn(M4, 4, 4, 4, 2)),
+	privateGamePlayerWonStarCheckColumn(M4, 4, 4, 4, 1).
 
 testGameOver :-
 	testPrivateDrawMatchGrid2(M),
