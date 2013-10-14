16936b1010d28d65de4061df304ddc0f1370f190
Martin Wetterwald <martin.wetterwald@insa-lyon.fr>
2013-10-14 13:30:10 +0200

    Improves hook and shell unit tests launcher. Adds "-q" option support.

--- src/runTest.sh
+++ src/runTest.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
+GITROOTDIR=`git rev-parse --show-toplevel`
+SOURCECODEDIR=$GITROOTDIR
+SOURCECODEDIR+=/src
+if [ "`pwd`" != $SOURCECODEDIR ]; then
+	cd "$SOURCECODEDIR"
+fi
+
 LOADFILE='swipl-load.gitlocal'
 EXECFILE='swipl-exec.gitlocal'
 
-echo "halt." | swipl -f testAll.pl -g testAll. 1> $EXECFILE 2> $LOADFILE
+ARG1=$1
+
+echo "halt." | swipl -s testAll.pl -g testAll. 1> $EXECFILE 2> $LOADFILE
 
 FAILED=0
 BUFFER=''
@@ -32,6 +41,9 @@ fi
 rm -f $LOADFILE $EXECFILE
 
 if [ $FAILED -eq 0 ]; then
+	if [ "$ARG1" != "-q" ]; then
+		echo -e "\033[1;32mAll tests \033[0m\033[1m\033[42mPASSED\033[0m\033[1;32m!\033[0m"
+	fi
 	exit 0
 fi
 
