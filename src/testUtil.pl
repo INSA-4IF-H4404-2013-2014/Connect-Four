d6cb40e9b20d5092985d390665a2958e081e39e9
Aline Martin <aline.martin@insa-lyon.fr>
2013-10-15 11:20:32 +0200

    no tracing when commit

--- src/testUtil.pl
+++ src/testUtil.pl
@@ -3,13 +3,13 @@
 
 %----------------------------
 % constant which indicates what is to trace (by default 'none' but can add the other )
-tracing('none').
+% tracing('none').
 
 % constant which indicates if tracing what happend when playing
-tracing('game').
+% tracing('game').
 
 % constant which indicates if printing an graphical interface for player s ia(redundant with 'game')
-tracing('graphInt').
+% tracing('graphInt').
 
 % constant which indicates if printing a textual interface for player s ia
 % tracing('txtInt').
