b0445f19e299349bc4e65e232f8d46f30fa8f7be
Guillaume Abadie <guillaume.abadie@gmail.com>
2013-10-15 20:59:02 +0200

    Ticket #3744: Refactors iaInferenceDatabase.pl

--- src/iaInferenceDatabase.pl
+++ src/iaInferenceDatabase.pl
@@ -4,11 +4,9 @@
 :- [gameCore].
 
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% DATABASE
-% free
-% 1 : the winner
-% 2 : the looser
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% KNOWN SHEMA DATABASE
 :- dynamic iaInferenceDB/1.
+
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% LEARN THE FIRST SHEMA
 % N W W W M
@@ -19,10 +17,3 @@
     [3, 0, 1],
     [4, 0, 0]
 ])).
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% HOW IS
-% test a player status (winner of looser)
-% iaInferencePlayerStatus(WinnerId, PlayerId, PlayerStatus).
-iaInferencePlayerStatus(X, X, 1).
-iaInferencePlayerStatus(X, Y, 2) :- gameOtherPlayer(X, Y).
--- src/testIaInferenceDatabase.pl
+++ src/testIaInferenceDatabase.pl
@@ -6,15 +6,4 @@
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% UNIT TESTS
 
-testIaInferencePlayerStatus :-
-    iaInferencePlayerStatus(1, 1, 1),
-    iaInferencePlayerStatus(2, 2, 1),
-    iaInferencePlayerStatus(2, 1, 2),
-    iaInferencePlayerStatus(1, 2, 2),
-    not(iaInferencePlayerStatus(1, 1, 2)),
-    not(iaInferencePlayerStatus(2, 2, 2)),
-    not(iaInferencePlayerStatus(2, 1, 1)),
-    not(iaInferencePlayerStatus(1, 2, 1)).
-
-testAllIaInferenceDatabase :-
-    test(testIaInferencePlayerStatus).
+testAllIaInferenceDatabase :- true.
