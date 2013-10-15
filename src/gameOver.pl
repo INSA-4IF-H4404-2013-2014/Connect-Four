e13c6d5c280dc4eb3107637b20baede1e8d336c6
Martin Wetterwald <martin.wetterwald@insa-lyon.fr>
2013-10-15 23:48:16 +0200

    Implements #3797: Make the number of aligned pawns to win dynamic.
    New NumberOfAlignedPawnsToWin/1 predicate, with initial value set to "4".

--- src/gameOver.pl
+++ src/gameOver.pl
@@ -11,8 +11,9 @@ gameGridIsFull([T|M]) :- length(T,N), linesNumber(LN), N == LN, gameGridIsFull(M
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%% Checks if there are 4 pawns of the same color in the 2 diagonals %%%%%%%%
 privateGamePlayerWonStarCheckDiagonalRoutine(Matrix, ReferenceColumn, CurrentColumn, TopLine, CurrentLine, Player, Direction) :-
+	numberOfAlignedPawnsToWin(AlignedPawnsToWin),
 	Delta is abs(ReferenceColumn - CurrentColumn),
-	Delta >= 4 ;
+	Delta >= AlignedPawnsToWin ;
 	gameGridGet(Matrix, CurrentColumn, CurrentLine, Player),
 	CurrentColumn2 is (CurrentColumn + Direction),
 	CurrentLine2 is (CurrentLine - 1),
@@ -37,8 +38,9 @@ privateGamePlayerWonStarCheckDiagonal(Matrix, LastColumnPlayed, TopLine, Player)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%% Checks if there are 4 pawns of the same color in the one line %%%%%%%%%%
 privateGamePlayerWonStarCheckLineRoutine(Matrix, ReferenceColumn, CurrentColumn, TopLine, Player, Direction) :-
+	numberOfAlignedPawnsToWin(AlignedPawnsToWin),
 	Delta is abs(ReferenceColumn - CurrentColumn),
-	Delta >= 4 ;
+	Delta >= AlignedPawnsToWin ;
 	gameGridGet(Matrix, CurrentColumn, TopLine, Player),
 	CurrentColumn2 is (CurrentColumn + Direction),
 	privateGamePlayerWonStarCheckLineRoutine(Matrix, ReferenceColumn, CurrentColumn2, TopLine, Player, Direction).
@@ -61,17 +63,12 @@ privateGamePlayerWonStarCheckLine(Matrix, LastColumnPlayed, TopLine, Player) :-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%% Checks if there are 4 pawns of the same color in one column %%%%%%%%%%%
 privateGamePlayerWonStarCheckColumn(Matrix, LastColumnPlayed, TopLine, CurrentLine, Player) :-
-	gameGridGet(Matrix, LastColumnPlayed, CurrentLine, Player) ->
-	(
-		(
-			Delta is (TopLine - CurrentLine),
-			Delta >= 3
-		) ;
-		(
-			CurrentLine2 is (CurrentLine - 1),
-			privateGamePlayerWonStarCheckColumn(Matrix, LastColumnPlayed, TopLine, CurrentLine2, Player)
-		)
-	).
+	numberOfAlignedPawnsToWin(AlignedPawnsToWin),
+	Delta is abs(TopLine - CurrentLine),
+	Delta >= AlignedPawnsToWin ;
+	gameGridGet(Matrix, LastColumnPlayed, CurrentLine, Player),
+	CurrentLine2 is (CurrentLine - 1),
+	privateGamePlayerWonStarCheckColumn(Matrix, LastColumnPlayed, TopLine, CurrentLine2, Player).
 
 privateGamePlayerWonStarCheckColumn(Matrix, LastColumnPlayed, TopLine, Player) :-
 	privateGamePlayerWonStarCheckColumn(Matrix, LastColumnPlayed, TopLine, TopLine, Player).
