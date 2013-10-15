b47a0105c3a720ab0d7be56b70edaf460446fd32
Guillaume Abadie <guillaume.abadie@gmail.com>
2013-10-15 10:47:42 +0200

    Adds interesting grid case

--- src/testGridCases.pl
+++ src/testGridCases.pl
@@ -8,3 +8,18 @@ testCaseGridColumn34([
     [],
     []
 ]).
+
+testCaseGridPlayer1IsWining1([
+    [],
+    [1],
+    [1,1,2],
+    [2,2,1,2],
+    [1,2,1],
+    [1,2],
+    [2,1,1]
+]).
+
+testCaseGridPlayer1Win1(R) :-
+    testCaseGridPlayer1IsWining1(R0),
+    gamePlay(R0, 6, 2, R1),
+    gamePlay(R1, 5, 1, R).
