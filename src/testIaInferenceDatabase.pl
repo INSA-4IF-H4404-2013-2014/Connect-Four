c09ccbc1699701b7fc8d75af20a96aecff0c17e6
Guillaume Abadie <guillaume.abadie@gmail.com>
2013-10-15 21:42:54 +0200

    Ticket #3744: Implements and tests iaInferenceConsultDatabase/5

--- src/testIaInferenceDatabase.pl
+++ src/testIaInferenceDatabase.pl
@@ -6,4 +6,13 @@
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% UNIT TESTS
 
-testAllIaInferenceDatabase :- true.
+testIaInferenceConsultDatabase :-
+    gameNewGrid(G0),
+    gamePlay(G0, 1, 2, G1),
+    gamePlay(G1, 4, 2, G2),
+    iaInferenceConsultDatabase(G0, 1, _, _, _, 3),
+    iaInferenceConsultDatabase(G1, 1, _, _, _, 3),
+    iaInferenceConsultDatabase(G2, 1, _, _, _, 6).
+
+testAllIaInferenceDatabase :-
+    test(testIaInferenceConsultDatabase).
