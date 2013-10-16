dfe930b4dae0320ec6b2eef57a41f9d8998bf1e5
Guillaume Abadie <guillaume.abadie@gmail.com>
2013-10-16 22:07:07 +0200

    Ticket #3744: Adds unit test testPrivateIaInferenceCleverMove1

--- src/testIaInferenceCleverMove.pl
+++ src/testIaInferenceCleverMove.pl
@@ -13,6 +13,11 @@ testPrivateIaInferenceCleverMove0 :-
     not(privateIaInferenceCleverMove(G, 1, [0, 1, 0], 3, 2, 1, 2, 1, 4)),
     not(privateIaInferenceCleverMove(G, 2, [2, 3, 0], 3, 2, 1, 2, 3, 2)).
 
+testPrivateIaInferenceCleverMove1 :-
+    gameNewGrid(G),
+    privateIaInferenceCleverMove(G, 1, [1, 0, 1], 1, 1, 1, 4, 2, 1),
+    privateIaInferenceCleverMove(G, 1, [1, 0, 1], 1, 1, 4, 1, 4, 1).
+
 testIsInferenceCleverMove :-
     gameNewGrid(G),
     testGridCaseEasySchema(S1),
@@ -20,4 +25,5 @@ testIsInferenceCleverMove :-
 
 testAllIaInferenceCleverMove :-
     test(testPrivateIaInferenceCleverMove0),
+    test(testPrivateIaInferenceCleverMove1),
     test(testIsInferenceCleverMove).
