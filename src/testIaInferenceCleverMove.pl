fd19fe74bda51767ff26715e4fb1890a22753429
Guillaume Abadie <guillaume.abadie@gmail.com>
2013-10-16 21:59:06 +0200

    Ticket #3744: Adds unit test testPrivateIaInferenceCleverMove0

--- src/testIaInferenceCleverMove.pl
+++ src/testIaInferenceCleverMove.pl
@@ -5,10 +5,19 @@
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TEST CASES
 
+%privateIaInferenceCleverMove(_, _, [_,_,0], _, _, SubColumnId, MetaDistance, SubColumnId, MetaDistance).
+
+testPrivateIaInferenceCleverMove0 :-
+    gameNewGrid(G),
+    privateIaInferenceCleverMove(G, 1, [0, 1, 0], 3, 2, 1, 2, 1, 2),
+    not(privateIaInferenceCleverMove(G, 1, [0, 1, 0], 3, 2, 1, 2, 1, 4)),
+    not(privateIaInferenceCleverMove(G, 2, [2, 3, 0], 3, 2, 1, 2, 3, 2)).
+
 testIsInferenceCleverMove :-
     gameNewGrid(G),
     testGridCaseEasySchema(S1),
     isInferenceCleverMove(G, 1, S1, 1, 1, 4, 1).
 
 testAllIaInferenceCleverMove :-
+    test(testPrivateIaInferenceCleverMove0),
     test(testIsInferenceCleverMove).
