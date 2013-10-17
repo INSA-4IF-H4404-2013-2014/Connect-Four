abae79fba0a3207f049f6ac5aac874d37dd818dd
Guillaume Abadie <guillaume.abadie@gmail.com>
2013-10-17 22:40:49 +0200

    Ticket #3744: Lets the IA use game{Winning,Survive}Moves

--- src/iaInference.pl
+++ src/iaInference.pl
@@ -1,4 +1,5 @@
 
+:- [gameSmart].
 :- [playerRandom].
 :- [iaInferenceSchema].
 :- [iaInferenceCleverMove].
@@ -8,9 +9,22 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% INCLUDES
 
 iaInference(Grid, PlayerId, ColumnId) :-
+    gameWinningMoves(Grid, PlayerId, ObviousMove)
+    -> (
+        [ColumnId|_] = ObviousMove
+    );
+    gameSurviveMoves(Grid, PlayerId, SurviveMoves)
+    -> (
+        (SurviveMoves > 1)
+        -> (
+            % we failed => we have to understand why
+            listFetch(SurviveMoves, 1, ColumnId)
+        ); (
+            listFetch(SurviveMoves, 1, ColumnId)
+        )
+    );
     iaInferenceConsultDatabase(Grid, PlayerId, Schema, PosX, PosY, _)
-    ->
-    (
+    -> (
         isInferenceCleverMove(Grid, PlayerId, Schema, PosX, PosY, ColumnId, _)
     ); (
         playerRandom(Grid, PlayerId, ColumnId)
