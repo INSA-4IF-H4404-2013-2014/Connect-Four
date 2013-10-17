797359372e72c4154a047093a6c90357c1aab959
Guillaume Abadie <guillaume.abadie@gmail.com>
2013-10-17 16:00:57 +0200

    Ticket #3744: Implements iaSchemaPopulate/4

--- src/iaSchemaProcessing.pl
+++ src/iaSchemaProcessing.pl
@@ -1,5 +1,5 @@
 
-:- [gameCore].
+:- [gameOver].
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% CREATE A SCHEMA FILLED WITH KILLER MOVES ONLY
@@ -19,3 +19,43 @@ iaSchemaInsertPawn(Schema, X, Y, Schema) :-
 
 iaSchemaInsertPawn(Schema, X, Y, [[X, Y, 1]|Schema]) :-
     not(member([X, Y, _], Schema)).
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% POPULATE SCHEMA
+% Insert al aligned pawn in the schema
+% iaSchemaPopulate(Grid, KillerMoves, PlayerId, RSchema)
+
+privateIaSchemaPopulateByY(_, _, Schema, Schema, _, 0).
+
+privateIaSchemaPopulateByY(Grid, PlayerId, Schema, RSchema, X, Y) :-
+    Y1 is Y - 1,
+    (
+        (
+            gameGridGet(Grid, X, Y, PlayerId),
+            gameIsAligned(Grid, X, Y, PlayerId)
+        ) -> (
+            iaSchemaInsertPawn(Schema, X, Y, NewSchema)
+        ) ; (
+            Schema = NewSchema
+        )
+    ),
+    privateIaSchemaPopulateByY(Grid, PlayerId, NewSchema, RSchema, X, Y1).
+
+
+privateIaSchemaPopulateByX(_, _, Schema, Schema, 0).
+
+privateIaSchemaPopulateByX(Grid, PlayerId, Schema, RSchema, X) :-
+    X1 is X - 1,
+    linesNumber(LineCount),
+    privateIaSchemaPopulateByY(Grid, PlayerId, Schema, NewSchema, X, LineCount),
+    privateIaSchemaPopulateByX(Grid, PlayerId, NewSchema, RSchema, X1).
+
+
+iaSchemaPopulate(Grid, KillerMoves, PlayerId, RSchema) :-
+    (
+        gamePlaySequence(Grid, KillerMoves, PlayerId, DominationGrid),
+        iaSchemaCreate(Grid, KillerMoves, NewSchema)
+    ) -> (
+        columnsNumber(ColumnCount),
+        privateIaSchemaPopulateByX(DominationGrid, PlayerId, NewSchema, RSchema, ColumnCount)
+    ), !.
