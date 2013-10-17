989d929c243061b4a5f6230548e11feb3964490c
Guillaume Abadie <guillaume.abadie@gmail.com>
2013-10-17 14:53:16 +0200

    Ticket #3744: Implements iaSchemaInsertPawn/4

--- src/iaSchemaProcessing.pl
+++ src/iaSchemaProcessing.pl
@@ -10,3 +10,12 @@ iaSchemaCreate(Grid, [X|KillerMoves], [[X,Y,0]|Schema]) :-
     iaSchemaCreate(Grid, KillerMoves, Schema),
     gameColumnHeight(Grid, X, ColumnHeight),
     Y is ColumnHeight + 1.
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% INSERT A PAWN IN A SHEMA IF NOT ALREADY INSERTED
+
+iaSchemaInsertPawn(Schema, X, Y, Schema) :-
+    member([X, Y, _], Schema).
+
+iaSchemaInsertPawn(Schema, X, Y, [[X, Y, 1]|Schema]) :-
+    not(member([X, Y, _], Schema)).
--- src/testIaSchemaProcessing.pl
+++ src/testIaSchemaProcessing.pl
@@ -10,5 +10,13 @@ testIaSchemaCreate :-
     testCaseGridPlayer1IsWining1(R),
     iaSchemaCreate(R, [5,6], [[5,4,0],[6,3,0]]).
 
+testIaSchemaInsertPawn :-
+    iaSchemaInsertPawn([], 3, 2, [[3, 2, 1]]),
+    iaSchemaInsertPawn([[3, 2, 1]], 3, 2, [[3, 2, 1]]),
+    iaSchemaInsertPawn([[3, 2, 0]], 3, 2, [[3, 2, 0]]),
+    iaSchemaInsertPawn([[3, 2, 1]], 2, 2, [[2, 2, 1], [3, 2, 1]]),
+    not(iaSchemaInsertPawn([[3, 2, 1]], 3, 2, [[3, 2, 1], [3, 2, 1]])).
+
 testAllIaSchemaProcessing :-
-    test(testIaSchemaCreate).
+    test(testIaSchemaCreate),
+    test(testIaSchemaInsertPawn).
