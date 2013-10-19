2d322c65fd9ff1b1a3fa2f2c8a34f72907215ebf
Remi Domingues <remi.domingues@insa-lyon.fr>
2013-10-19 22:18:41 +0200

    Player Tree Explorer part 1 implemented / Complete debug and testing still todo / Evaluation function still todo (See bob)

--- src/playerTreeExplorer2.pl
+++ src/playerTreeExplorer2.pl
@@ -14,37 +14,22 @@
 % Fonctions requises par l'algo MIN-MAX
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%
-% Fonction simulatePlay
-% 		  si l'on peut jouer dans la colonne nextCol
-%		      jouer
-%         	  évaluation = le résultat de la fonction d'évaluation sur la grille passée en paramètre
-%		  sinon
-%			  évaluation = 0
-%         fin
-
-
-
-si l'on peut jouer
-%			jouer dans la colonne nextCol
-%		 fin
-%        	s'appeler soit-même avec une grille copiée de la grille reçue, avec nextcol = nextcol + 1
-%        si l'on peut jouer
-%        	ajouter en tête de la liste evaluations la valeur évaluation reçue par l'appel du fils
-%        sinon
-%           ajouter en tête de la liste evaluations 0
-%%%%%%%%%%%%%%%%%%%%
-
-%%%%%%%%%%%%%%%%%%%%
 % getMax(List, Max) <=> Max = getMax(List)
-% Retourne le plus grand élément présent dans une liste
+% Retourne le plus grand élément présent dans une liste (x est une valeur à skipper)
 %%%%%%%%%%%%%%%%%%%%
+getMax([Max], Max).
+getMax([x|L], Max) :- getMax(L, Max).
+getMax([T|L], Max) :- getMax(L, Max1), (T >= Max1, Max = T; T < Max1, Max = Max1).
 
 
 
 %%%%%%%%%%%%%%%%%%%%
 % getMin(List, Min) <=> Min = getMin(List)
-% Retourne le plus petit élément présent dans une liste
+% Retourne le plus petit élément présent dans une liste (x est une valeur à skipper)
 %%%%%%%%%%%%%%%%%%%%
+getMin([Min], Min).
+getMin([x|L], Min) :- getMin(L, Min).
+getMin([T|L], Min) :- getMin(L, Min1), (T <= Min1, Min = T; T > Min1, Min = Min1).
 
 
 
@@ -52,35 +37,35 @@ si l'on peut jouer
 % indexOf(List, Elt, Idx) <=> Idx = indexOf(List, Elt)
 % Retourne l'index du premier élément égal à Elt présent dans la liste
 %%%%%%%%%%%%%%%%%%%%
-indexOf([], Elt, Idx)
+indexOf([Elt|L], Elt, 0).
+indexOf([T|L], Elt, Idx) :- indexOf(L, Elt, Idx1), Idx is Idx + 1.
 
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % ALGO MIN-MAX
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% si hauteur maximale atteinte
-%     si nextCol = nbMaxCol : P1
-%         %JOUER un pion dans la colonne nextCol si possible
-%         Evaluations est une liste contenant pour seul élément le résultat de la fonction évaluation (ou 0 si on ne peut jouer)
-%     sinon : P2
-%		  %JOUER un pion dans la colonne nextCol si possible
-%         ajouter en tête de la liste Evaluations le résultat de la fonction évaluation (ou 0 si on ne peut jouer)
-%     fin
-% sinon
-%     si nextCol = nbMaxCol : P3
-%		  %JOUER un pion dans la colonne nextCol si possible
-%         s'appeler soit-même avec une grille copiée de la grille jouée, avec nextcol = 1 et depth = depth + 1
-%         si idplayer = currentplayer : P4
-%             Evaluations est (Désactivé : mettre [MINMAX] pour activer : %une liste ayant pour seul élément%) le max de la liste evaluations1
-%         sinon : P5
-%             Evaluations est %une liste ayant pour seul élément% le min de la liste evaluations1
+% si nextCol = nbMaxCol + 1 : P1
+%		évaluations est une liste vide
+% fin
+% si profondeur maximale atteinte : P2
+%     JOUER un pion dans la colonne nextCol si possible
+%     ajouter en tête de la liste Evaluations le résultat de la fonction évaluation (ou x si on ne peut jouer)
+% fin
+% sinon : P3
+%     copier la grille reçue
+%     si on peut jouer
+%		  JOUER un pion dans la colonne nextCol
+%         s'appeler soit-même avec la grille jouée, avec nextcol = 1 et depth += 1
+%         si idplayer = currentplayer
+%             ajouter en tête de la liste evaluations le max de la liste evaluations1 initialisée par l'appel au fils
+%         sinon
+%             ajouter en tête de la liste evaluations le min de la liste evaluations1 initialisée par l'appel au fils
 %         fin
-%     sinon : P6
-%        %JOUER un pion dans la colonne nextCol si possible
-%        s'appeler soit-même avec une grille copiée de la grille reçue, avec nextcol = nextcol + 1
-%        ajouter en tête de la liste evaluations la valeur évaluation reçue par l'appel du fils
+%	  sinon
+%         ajouter en tête de la liste evaluations x
 %     fin
+%     s'appeler soit-même avec la grille copiée de la grille reçue, avec nextcol = nextcol + 1
 % fin
 %%%%%
 % Appel Initial :
@@ -94,56 +79,71 @@ maxDepth(3).
 
 
 %%%%%%%%%%%%%%%%%%%%
-% P0 : Jouer un coup si possible
+% P1
+% si nextCol = nbMaxCol + 1 : P1
+%		évaluations est une liste vide
+% fin
 %%%%%%%%%%%%%%%%%%%%
-privatePlayerTreeExplorer(Grid, IdPlayer, NextCol, [Evaluation], Depth, CurrentPlayer) :-
-	maxDepth(Depth) ->
-	columnsNumber(NextCol) ->
-	evaluate(Grid, NumCol, CurrentPlayer, Evaluation).
-
-
-%%%%%%%%%%%%%%%%%%%%
-% P1 : Profondeur maximale atteinte : on évalue la grille
-%%%%%%%%%%%%%%%%%%%%
-privatePlayerTreeExplorer(Grid, IdPlayer, NextCol, [Evaluation], Depth, CurrentPlayer) :-
-	maxDepth(Depth) ->
-	columnsNumber(NextCol) ->
-	evaluate(Grid, NumCol, CurrentPlayer, Evaluation).
+privatePlayerTreeExplorer(Grid, IdPlayer, NextCol, [], Depth, CurrentPlayer) :-
+	NextCol1 is NextCol-1 ->
+	columnsNumber(NextCol).
 	
 	
 %%%%%%%%%%%%%%%%%%%%
 % P2
+% si profondeur maximale atteinte : P2
+%     JOUER un pion dans la colonne nextCol si possible
+%     ajouter en tête de la liste Evaluations le résultat de la fonction évaluation (ou x si on ne peut jouer)
+% fin
 %%%%%%%%%%%%%%%%%%%%
 privatePlayerTreeExplorer(Grid, IdPlayer, NextCol, [Evaluation|L], Depth, CurrentPlayer) :-
 	maxDepth(Depth) ->
-	evaluate(Grid, NumCol, CurrentPlayer, Evaluation).
+	gameColumnHeight(Grid, NextCol, ColumnHeight) ->
+	(
+		linesNumber(ColumnHeight) ->
+		gamePlay(Grid, NextCol, CurrentPlayer, GridResult) ->
+		evaluate(GridResult, NextCol, CurrentPlayer, Evaluation)
+		;
+		Evaluation = x
+	).
 	
 	
 %%%%%%%%%%%%%%%%%%%%
 % P3 : 
-%%%%%%%%%%%%%%%%%%%%
-privatePlayerTreeExplorer(Grid, IdPlayer, NextCol, MinMax, Depth, CurrentPlayer) :-
-	columnsNumber(NextCol) ->
-	copy_term(Grid, Grid1) ->
-	Depth1 is Depth + 1 ->
-	privatePlayerTreeExplorer(Grid1, IdPlayer, 1, EvaluationsR, Depth1, IdPlayer) ->
-	(
-		%P4
-		IdPlayer = CurrentPlayer ->
-		getMax(EvaluationsR, MinMax)
-		;
-		%P5
-		getMin(EvaluationsR, MinMax)
-	).
-	
-
-%%%%%%%%%%%%%%%%%%%%
-% P6 : 
+%     copier la grille reçue
+%     si on peut jouer
+%		  JOUER un pion dans la colonne nextCol
+%         s'appeler soit-même avec la grille jouée, avec nextcol = 1 et depth += 1
+%         si idplayer = currentplayer : P4
+%             ajouter en tête de la liste evaluations le max de la liste evaluations1 initialisée par l'appel au fils
+%         sinon : P5
+%             ajouter en tête de la liste evaluations le min de la liste evaluations1 initialisée par l'appel au fils
+%         fin
+%	  sinon
+%         ajouter en tête de la liste evaluations x
+%     fin
+%     s'appeler soit-même avec la grille copiée de la grille reçue, avec nextcol = nextcol + 1
 %%%%%%%%%%%%%%%%%%%%
 privatePlayerTreeExplorer(Grid, IdPlayer, NextCol, [Evaluation|L], Depth, CurrentPlayer) :-
 	copy_term(Grid, Grid1) ->
-	NextCol1 is NextCol + 1 ->
-	privatePlayerTreeExplorer(Grid1, IdPlayer, NextCol, Evaluation, Depth, IdPlayer).
+	gameColumnHeight(Grid, NextCol, ColumnHeight) ->
+	(
+		linesNumber(ColumnHeight) ->
+		gamePlay(Grid, NextCol, CurrentPlayer, GridResult) ->
+		Depth1 is Depth + 1 ->
+		gameOtherPlayer(IdPlayer, NextPlayerId)
+		privatePlayerTreeExplorer(GridResult, IdPlayer, 1, EvaluationsResult, Depth1, NextPlayerId) ->
+		(
+			IdPlayer = CurrentPlayer ->
+			getMax(EvaluationsResult, Evaluation)
+			;
+			getMin(EvaluationsResult, Evaluation)
+		) ->
+		;
+		Evaluation is x
+	) ->
+	nextCol1 is nextCol + 1 ->
+	privatePlayerTreeExplorer(Grid1, IdPlayer, nextCol1, MinMax, Depth, CurrentPlayer).
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
