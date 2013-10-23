8004372af140eab235700e61feb6c66c556b5364
Guillaume Abadie <guillaume.abadie@gmail.com>
2013-10-23 22:40:31 +0200

    Factorises in playerRandom.

--- src/playerRandom.pl
+++ src/playerRandom.pl
@@ -7,26 +7,32 @@
 % ColumnIndexWanted is the column of the move chosen by the Player (return val)
 playerRandom(Matrix, _, ColumnIndexWantedMove) :-
 	gameRemainingPlays(Matrix, Indexes),
-	length(Indexes, NumberOfPossibleMoves),
-	%Please don't touch this code. There is a bug on Ubuntu, only the form
-	%"A is random(X)" works. Cannot use other forms of random.
-	%http://comments.gmane.org/gmane.comp.ai.prolog.swi/15929
-	MoveWantedTmp is random(NumberOfPossibleMoves),
-	MoveWanted is (MoveWantedTmp + 1),
-	listFetch(Indexes, MoveWanted, ColumnIndexWantedMove).
-
+    privatePlayerRandom(Indexes, ColumnIndexWantedMove).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%%%%%%%%%%%%% Lauches the Player Random Smart and get its move decision %%%%%%%%%%%%%%
+
+%%%%%%%%%% Lauches the Player Random Smart and get its move decision %%%%%%%%%%%
 % Matrix is the game grid
 % PlayerId is the player number
 % ColumnId is the index of the column in which the AI wants to play
 
 % Play if the player has or winning move or can counter the opponent in the appropriate column
 playerRandomSmart(Matrix, PlayerId, ColumnId) :-
-	gameObviousMove(Matrix, PlayerId, ColumnId).
+	gameObviousMove(Matrix, PlayerId, ColumnId) ->
+        true;
+        playerRandom(Matrix, PlayerId, ColumnId).
 
-% If no winning or counter move is available, play a random move
-playerRandomSmart(Matrix, PlayerId, ColumnId) :-
-	playerRandom(Matrix, PlayerId, ColumnId).
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% PRIVATE
+
+privatePlayerRandom(ColumnIds, ColumnId) :-
+    %Please don't touch this code. There is a bug on Ubuntu, only the form
+    %"A is random(X)" works. Cannot use other forms of random.
+    %http://comments.gmane.org/gmane.comp.ai.prolog.swi/15929
+    length(ColumnIds, NumberOfPossibleMoves),
+    MoveWantedTmp is random(NumberOfPossibleMoves),
+    MoveWanted is (MoveWantedTmp + 1),
+    listFetch(ColumnIds, MoveWanted, ColumnId).
