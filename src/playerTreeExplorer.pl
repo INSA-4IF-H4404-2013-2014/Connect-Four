26e534f3df5cc17eec766741ed8d75d4126d17b1
Guillaume Abadie <guillaume.abadie@gmail.com>
2013-10-24 19:26:43 +0200

    Removes comments generating build-time errors

--- src/playerTreeExplorer.pl
+++ src/playerTreeExplorer.pl
@@ -19,7 +19,7 @@ removeXValues([T|L], [T|L1]) :- removeXValues(L, L1).
 
 %%%%%%%%%%%%%%%%%%%%
 % getMax(List, Max) <=> Max = getMax(List)
-% Retourne le plus grand élément présent dans une liste (x est une valeur à skipper)
+% test the maximum in a list
 %%%%%%%%%%%%%%%%%%%%
 getMax([Max], Max).
 getMax([T|L], Max) :- getMax(L, Max1), (T >= Max1, Max = T; T < Max1, Max = Max1).
@@ -28,7 +28,7 @@ getMax([T|L], Max) :- getMax(L, Max1), (T >= Max1, Max = T; T < Max1, Max = Max1
 
 %%%%%%%%%%%%%%%%%%%%
 % getMin(List, Min) <=> Min = getMin(List)
-% Retourne le plus petit élément présent dans une liste (x est une valeur à skipper)
+% test the minimum in a list
 %%%%%%%%%%%%%%%%%%%%
 getMin([Min], Min).
 getMin([x|L], Min) :- getMin(L, Min).
@@ -37,15 +37,15 @@ getMin([T|L], Min) :- getMin(L, Min1), (T =< Min1, Min = T; T > Min1, Min = Min1
 
 %%%%%%%%%%%%%%%%%%%%
 % indexOf(List, Elt, Idx) <=> Idx = indexOf(List, Elt)
-% Retourne l'index du premier élément égal à Elt présent dans la liste
+% test the index of an element in a list
 %%%%%%%%%%%%%%%%%%%%
 indexOf([Elt|_], Elt, 1).
 indexOf([_|L], Elt, Idx) :- indexOf(L, Elt, Idx1), Idx is Idx1 + 1.
 
 
 %%%%%%%%%%%%%%%%%%%%
-% countItem(List, Elt, NbOcc) <=> NbOcc = nombre d'occurences de Elt dans List
-% Retourne le nombre d'occurences de Elt dans List
+% countItem(List, Elt, NbOcc)
+% test the number of occurence in a list
 %%%%%%%%%%%%%%%%%%%%
 countItem([], _, 0).
 countItem([Elt|L], Elt, NbOcc) :- countItem(L, Elt, NbOcc1), NbOcc is NbOcc1 + 1.
@@ -53,8 +53,7 @@ countItem([_|L], Elt, NbOcc) :- countItem(L, Elt, NbOcc).
 
 
 %%%%%%%%%%%%%%%%%%%%
-% countNegative(List, NbOcc) <=> NbOcc = nombre d'occurences de nombre negatigs
-% Retourne le nombre d'occurences de nombre negatifs
+% countNegative(List, NbOcc)
 %%%%%%%%%%%%%%%%%%%%
 countNegative([], 0).
 countNegative([T|L], NbOcc) :- countNegative(L, NbOcc1), (T < 0, NbOcc is NbOcc1 + 1 ; NbOcc is NbOcc1).
@@ -65,8 +64,7 @@ countNegative([T|L], NbOcc) :- countNegative(L, NbOcc1), (T < 0, NbOcc is NbOcc1
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Évaluation d'une grille de jeu
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% evaluate a play%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Function to use :
 % evaluatePlay(Grid, ColumnId, PlayerId, MaxScore)
 
@@ -138,17 +136,12 @@ evaluateOtherPlayer(Matrix, ColumnId, PlayerId, MaxWin) :-
 	getMax([RColumn, RLine, RDiago1, RDiago2], MaxWin).
 	
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%					!!!!!!!!!!! CAUTION !!!!!!!!!!	
-
+%					!!!!!!!!!!! CAUTION !!!!!!!!!!
 % All evalute predicates count the number of conscutive pawns in the line, column or diagonals
 % corresponding at the ColumnId.
-
 % THIS NUMBER INCLUDES THE PAWN PLAYED!
-
 % For example, if no pawns are aligned on line, evaluateLine(Matrix, ColumnId, PlayerId, Value)
 % will get Value = 1.
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
@@ -499,70 +492,22 @@ countDistanceDiago2Right(Matrix, ColumnRef, ColumnId, LineId, PlayerId, Value) :
 countDistanceDiago2Right(Matrix, ColumnRef, ColumnId, LineId, PlayerId, Value) :-
 	(ColumnId1 is ColumnId + 1, LineId1 is LineId - 1) ->
 		countDistanceDiago2Right(Matrix, ColumnRef, ColumnId1, LineId1, PlayerId, Value).
-		
-		
-	
+
+
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % ALGO MIN-MAX
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% si nextCol = nbMaxCol + 1 : P1
-%		évaluations est une liste vide
-% fin
-% si profondeur maximale atteinte : P2
-%     JOUER un pion dans la colonne nextCol si possible
-%     ajouter en tête de la liste Evaluations le résultat de la fonction évaluation (ou x si on ne peut jouer)
-%     s appeler soit-même avec la grille copiée de la grille reçue, avec nextcol = nextcol + 1
-% fin
-% sinon : P3
-%     copier la grille reçue
-%     si on peut jouer
-%		  JOUER un pion dans la colonne nextCol
-%	  	  si l on a gagne
-%			retourne 1000
-%		  sinon
-%         	s appeler soit-même avec la grille jouée, avec nextcol = 1 et depth += 1
-%         	si idplayer = currentplayer
-%         	    ajouter en tête de la liste evaluations le min (l'autre joueur a joué) de la liste evaluations1 initialisée par l'appel au fils
-%       	  sinon
-%       	      ajouter en tête de la liste evaluations le max (l'IA a joué) de la liste evaluations1 initialisée par l'appel au fils
-%       	  fin
-%	  sinon
-%         ajouter en tête de la liste evaluations x
-%     fin
-%     s appeler soit-même avec la grille copiée de la grille reçue, avec nextcol = nextcol + 1
-% fin
-%%%%%
-% Appel Initial :
-% On calcule le max d'évaluations
-% NumCol est l'indice du max
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%
-%Profondeur maximale de recherche
+% max search depth
 %%%%%%%%%%%%%%%%%%%%
 % The depth MUST BE an impair number
 maxDepth(5).
 
-
-%%%%%%%%%%%%%%%%%%%%
-% P1
-% si nextCol = nbMaxCol + 1 : P1
-%		évaluations est une liste vide
-% fin
-%%%%%%%%%%%%%%%%%%%%
 privatePlayerTreeExplorer(_, _, NextCol, [], _, _) :-
 	NextCol1 is NextCol-1 ->
 	columnsNumber(NextCol1).
-	
 
-%%%%%%%%%%%%%%%%%%%%
-% P2
-% si profondeur maximale atteinte : P2
-%     copier la grille reçue
-%     JOUER un pion dans la colonne nextCol si possible
-%     ajouter en tête de la liste Evaluations le résultat de la fonction évaluation (ou x si on ne peut jouer)
-%     s appeler soit-même avec la grille copiée de la grille reçue, avec nextcol = nextcol + 1
-% fin
-%%%%%%%%%%%%%%%%%%%%
 privatePlayerTreeExplorer(Grid, IdPlayer, NextCol, [Evaluation|L], Depth, CurrentPlayer) :-
 	maxDepth(Depth) ->
 	copy_term(Grid, Grid1) ->
@@ -577,27 +522,7 @@ privatePlayerTreeExplorer(Grid, IdPlayer, NextCol, [Evaluation|L], Depth, Curren
 	) ->
 	NextCol1 is NextCol + 1 ->
 	privatePlayerTreeExplorer(Grid1, IdPlayer, NextCol1, L, Depth, CurrentPlayer).
-	
-	
-%%%%%%%%%%%%%%%%%%%%
-% P3 : 
-%     copier la grille reçue
-%     si on peut jouer
-%		  JOUER un pion dans la colonne nextCol
-%	  	  si l on a gagne
-%			retourne 1000
-%		  sinon
-%	         s appeler soit-même avec la grille jouée, avec nextcol = 1 et depth += 1
-%	         si idplayer = currentplayer : P4
-%	             ajouter en tête de la liste evaluations le min de la liste evaluations1 initialisée par l'appel au fils
-%	         sinon : P5
-%	     	        ajouter en tête de la liste evaluations le max de la liste evaluations1 initialisée par l'appel au fils
-%     	    fin
-%	  sinon
-%         ajouter en tête de la liste evaluations x
-%     fin
-%     s appeler soit-même avec la grille copiée de la grille reçue, avec nextcol = nextcol + 1
-%%%%%%%%%%%%%%%%%%%%
+
 privatePlayerTreeExplorer(Grid, IdPlayer, NextCol, [Evaluation|L], Depth, CurrentPlayer) :-
 	copy_term(Grid, Grid1) ->
 	gameColumnHeight(Grid, NextCol, ColumnHeight) ->
@@ -624,7 +549,6 @@ privatePlayerTreeExplorer(Grid, IdPlayer, NextCol, [Evaluation|L], Depth, Curren
 				IdPlayer == CurrentPlayer ->
 				getMin(EvaluationsResult1, Evaluation)
 				;
-				%Si une branche donne lieu a des defaites sur plusieurs coups -> l'handicaper par un score negatif
 				countNegative(EvaluationsResult1, N) ->
 				(
 					N >= 2 ->
@@ -639,8 +563,8 @@ privatePlayerTreeExplorer(Grid, IdPlayer, NextCol, [Evaluation|L], Depth, Curren
 	) ->  
 	NextCol1 is NextCol + 1 ->
 	privatePlayerTreeExplorer(Grid1, IdPlayer, NextCol1, L, Depth, CurrentPlayer).
-	
-	
+
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Initial call
 playerTreeExplorer(Grid, PlayerId, NumCol) :-	
